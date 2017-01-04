ffa8fee51eae8772ebec4db930b7a2a0553c5a89
David Reyes Pucheta <davidpucheta@gmail.com>
2017-01-04 13:32:47 -0600

    Terminar Código

--- evaluacion-BP01.js
+++ evaluacion-BP01.js
@@ -14,5 +14,21 @@ sumasIguales( [ 1, 2, 3, 4, 1, 1 ] ); // true
 
 function sumaEsPar(arr){
 
+    var pares = 0;
+    var impares = 0;
 
+    for (var i=0; i < arr.length; i++){
+        if ( (arr[i] % 2) === 0){
+            pares += arr[i];
+         } else {
+             impares += arr[i];
+         }
+    }
+
+    return pares === impares ? true : false;
 }
+
+sumaEsPar( [ 10, 5, 5 ] ); // true 
+sumaEsPar( [ 2, 1, 1, -1, 3, 4, 10 ] ); // false 
+sumaEsPar( [ -4, 10, -6 ] ); // true 
+sumaEsPar( [ 1, 2, 3, 4, 1, 1 ] ); // true
