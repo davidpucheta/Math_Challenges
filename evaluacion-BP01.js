9dcf5b49e8490f70c0a8067300c370fbf9285d05
David Reyes Pucheta <davidpucheta@gmail.com>
2017-01-04 13:20:31 -0600

    Agregar Evaluación BP 01

---
+++ evaluacion-BP01.js
@@ -0,0 +1,13 @@
+/*Instrucciones
+Realiza una función que reciba un arreglo de enteros e indique 
+si la suma de los números pares es igual a la de los números 
+impares dentro del arreglo.  
+
+
+EJEMPLO: 
+sumasIguales( [ 10, 5, 5 ] ); // true 
+sumasIguales( [ 2, 1, 1, -1, 3, 4, 10 ] ); // false 
+sumasIguales( [ -4, 10, -6 ] ); // true 
+sumasIguales( [ 1, 2, 3, 4, 1, 1 ] ); // true
+
+*/
