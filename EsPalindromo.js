78d450cf58361c511ee932d1e9d6c66761e963fd
David Reyes Pucheta <davidpucheta@gmail.com>
2017-01-06 13:35:11 -0600

    Añadir casos de prueba

--- EsPalindromo.js
+++ EsPalindromo.js
@@ -1,4 +1,9 @@
 function esPalindromo(str){
-        var revStr = str.split("").reverse().join(""); 
-        return str === revStr ? true : false
+        var revStr = str.replace(" ","").split("").reverse().join(""); 
+        return str === revStr ? true : false;
 }
+
+esPalindromo("ana"); // true 
+esPalindromo("mala"); // false 
+esPalindromo("anita lava la tina"); // true 
+esPalindromo("La ruta nos aporto otro paso natural"); // true
