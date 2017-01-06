eb3f3b375e3a0c7c77ba04b2a4a84e05d5ca90dd
David Reyes Pucheta <davidpucheta@gmail.com>
2017-01-06 14:00:29 -0600

    Añadir instrucciones

---
+++ arregloContiene.js
@@ -0,0 +1,5 @@
+/*Instrucciones
+Escribe una función o método que detecte si en un arreglo un número se repite, al menos, 
+cierta cantidad de veces. La función debe recibir el arreglo, 
+el número que se quiere detectar y la cantidad de veces mínimas que debe aparecer.
+*/
