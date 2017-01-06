62917c28d270a3fd8ef0fed15e886133eebb7f3a
David Reyes Pucheta <davidpucheta@gmail.com>
2017-01-06 13:36:01 -0600

    Palindromo JS terminado

--- EsPalindromo.js
+++ EsPalindromo.js
@@ -1,9 +1,10 @@
 function esPalindromo(str){
-        var revStr = str.replace(" ","").split("").reverse().join(""); 
+        str = str.replace(/\s/g,"").toLowerCase();
+        var revStr = str.split("").reverse().join(""); 
         return str === revStr ? true : false;
 }
 
-esPalindromo("ana"); // true 
-esPalindromo("mala"); // false 
-esPalindromo("anita lava la tina"); // true 
-esPalindromo("La ruta nos aporto otro paso natural"); // true
+console.log(esPalindromo("ana")); // true 
+console.log(esPalindromo("mala")); // false 
+console.log(esPalindromo("anita lava la tina")); // true 
+console.log(esPalindromo("La ruta nos aporto otro paso natural")); // true
