b02707434e56936a4ebd9982a904c62a702ad4e2
David Reyes Pucheta <davidpucheta@gmail.com>
2017-01-06 14:07:28 -0600

    agregar casos de prueba

--- arregloContiene.js
+++ arregloContiene.js
@@ -6,9 +6,17 @@ el número que se quiere detectar y la cantidad de veces mínimas que debe apare
 
 function contiene(arr, n, r){
    var filtrados =  arr.filter(function(elemento){
-       return elemento = n;
+       return elemento === n;
    });
 
    return filtrados.length === r ? true : false;
 
 }
+
+
+var arreglo = [4, 5, 2, 4, 5, 9, 9, 4, 4];  
+
+console.log(contiene(arreglo, 4, 5) ); // Regresa false;
+console.log(contiene(arreglo, 4, 4) ); // Regresa true; 
+console.log(contiene(arreglo, 4, 3) );// Regresa true; 
+console.log(contiene(arreglo, 9, 2) );// Regresa true;
