754bdf5907a03c9abafe25e9bfd7e7fa24163c19
David Reyes Pucheta <davidpucheta@gmail.com>
2017-01-06 14:04:33 -0600

    Agregar función 'contiene'

--- arregloContiene.js
+++ arregloContiene.js
@@ -3,3 +3,12 @@ Escribe una función o método que detecte si en un arreglo un número se repite
 cierta cantidad de veces. La función debe recibir el arreglo, 
 el número que se quiere detectar y la cantidad de veces mínimas que debe aparecer.
 */
+
+function contiene(arr, n, r){
+   var filtrados =  arr.filter(function(elemento){
+       return elemento = n;
+   });
+
+   return filtrados.length === r ? true : false;
+
+}
